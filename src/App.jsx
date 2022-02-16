12f8f4165c5257796e6d12f2b20e1349b2fdd2e6
Erich Sauer <erich.sauer@gmail.com>
2022-02-15 18:36:55 -0800

    react front end to express backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,75 @@
+import { useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import request, { listenerCount } from 'superagent';
+// import { useCookies } from 'react-cookie';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const history = useHistory();
+  const [user, setUser] = useState(null);
+  const [sparkles, setSparkles] = useState([]);
+  const [sparkle, setSparkle] = useState('');
+  // const [cookies, setCookie] = useCookies();
+
+  const getSparkles = async () => {
+    const res = await request
+      .get('http://localhost:7890/api/v1/sparkles')
+      .withCredentials();
+    setSparkles(res.body.reverse());
+  };
+
+  const handleLogin = async () => {
+    window.location.assign('http://localhost:7890/api/v1/github/login');
+  };
+
+  const handleSparkle = async (e) => {
+    e.preventDefault();
+    const res = await request
+      .post('http://localhost:7890/api/v1/sparkles')
+      .withCredentials()
+      .send({ text: sparkle });
+    setSparkle('');
+    await getSparkles();
+  };
+
+  const handleLogOut = async () => {
+    const res = await request.delete(
+      'http://localhost:7890/api/v1/github/sessions'
+    );
+    console.log(res);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const res = await request
+        .get('http://localhost:7890/api/v1/github/dashboard')
+        .withCredentials();
+      setUser(res.body);
+    })();
+    (async () => await getSparkles())();
+  }, []);
+
+  return !user ? (
+    <button onClick={handleLogin}>login with github</button>
+  ) : (
+    <main>
+      <form onSubmit={handleSparkle}>
+        <fieldset>
+          <legend>welcome to glitter</legend>
+          <input
+            type="text"
+            placeholder="type yr thots"
+            value={sparkle}
+            onChange={(e) => setSparkle(e.target.value)}
+          />
+          <button type="submit">sparkle</button>
+        </fieldset>
+      </form>
+      <ul>
+        {sparkles.map((sparkle) => (
+          <li key={sparkle.id + sparkle.userId}>{sparkle.text}</li>
+        ))}
+      </ul>
+      <button onClick={handleLogOut}>log out</button>
+    </main>
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import { CookiesProvider } from 'react-cookie';
 
 render(
   <React.StrictMode>
-    <App />
+    {/* <CookiesProvider> */}
+    <Router>
+      <App />
+    </Router>
+    {/* </CookiesProvider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
